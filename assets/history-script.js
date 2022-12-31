0ee7b691035f14d1d48a7891f4e0c5be06f5c37e
Muhammad Ilyas <muhammad.ilyas@jubelio.com>
2022-12-31 14:59:54 +0700

    fix right sidebar

--- assets/history-script.js
+++ assets/history-script.js
@@ -89,27 +89,22 @@
             var awardedFellowshipRecord = document.getElementById('awarded-fellowship-record');
             var accoladesAwwardRecord = document.getElementById('acolades-awward-record');
 
+            const launchedProduct = entries[0].target.getAttribute('launchedProduct')
+            const fellowshipAwarded = entries[0].target.getAttribute('fellowshipAwarded')
+            const teamMember = entries[0].target.getAttribute('teamMember')
+            const awardAccolades = entries[0].target.getAttribute('awardAccolades')
+
             const id = entries[0].target.id
             if(entries[0].isIntersecting === true){
-                if(id.includes('team-member')) {
-                    memberTeamRecord.innerHTML = id.split('-')[2];
-                } else if (id.includes('launched-product')) {
-                    launchedProductRecord.innerHTML = id.split('-')[2];
-                } else if (id.includes('fellowship-awarded')) {
-                    awardedFellowshipRecord.innerHTML = id.split('-')[2];
-                } else if (id.includes('awward-acolades')) {
-                    accoladesAwwardRecord.innerHTML = id.split('-')[2];
-                }
+                memberTeamRecord.innerHTML = teamMember;
+                launchedProductRecord.innerHTML = launchedProduct;
+                awardedFellowshipRecord.innerHTML = fellowshipAwarded;
+                accoladesAwwardRecord.innerHTML = awardAccolades;
 
             }
         }, { threshold: [1] });
 
-        var teamMembers = document.querySelectorAll('[id^="team-member"]');
-        var launchedProducts = document.querySelectorAll('[id^="launched-product"]');
-        var fellowshipAwarded = document.querySelectorAll('[id^="fellowship-awarded"]');
-        var awardAccolades = document.querySelectorAll('[id^="awward-acolades"]');
-
-        const arrays = [...teamMembers, ...launchedProducts, ...fellowshipAwarded, ...awardAccolades]
+        var arrays = document.querySelectorAll('[class^="content-wrapper"]');
 
         arrays.forEach(function (element) {
             observer.observe(element);
@@ -123,25 +118,14 @@
     years.forEach(function (year) {
         year.addEventListener('click', function (e) {
             e.preventDefault();
-
-            // // get all element class years
-            // const years = document.querySelectorAll(`.years`);
-            // // remove class .active from all element
-            // years.forEach(function (year) {
-            //     year.classList.remove('active');
-            // })
-
-            // // add class .active into parent 
-            // const parent = e.target.parentNode;
-            // parent.classList.add('active');
-
             // scroll to year element
             const year = e.target.id;
             const yearElement = document.getElementById(year.split('-')[1]);
-            yearElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-                top: 35
+
+            // scroll to year element and add gap 100px
+            window.scrollTo({
+                top: yearElement.offsetTop - 250,
+                behavior: 'smooth'
             });
         })
     }
